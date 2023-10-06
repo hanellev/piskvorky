8c4b97f707f429f27336645d3739e1bce96be593
Hana Vavrušková <hana.vavruskova@gmail.com>
2023-10-07 00:26:36 +0200

    Úprava funkce pro zobrazování koleček a křížků

--- hra.js
+++ hra.js
@@ -1,46 +1,46 @@
-let currentPlayer = 'circle';
+let playingPlayer = 'circle';
 
 document.querySelector('#menu__hraje-icon').classList.add('menu__hraje-circle')
 
-const whenClicked = (event) => {
-	if (currentPlayer === 'circle') {
+const cellClick = (event) => {
+	if (playingPlayer === 'circle') {
         event.target.classList.add('cell__circle');
-        document.querySelector('#menu__hraje-icon').classList.remove('menu-hraje__circle')
-        document.querySelector('#menu__hraje-icon').classList.add('menu-hraje__cross')
-        currentPlayer = 'cross';
+        document.querySelector('#menu__hraje-icon').classList.remove('menu__hraje-circle')
+        document.querySelector('#menu__hraje-icon').classList.add('menu__hraje-cross')
+        playingPlayer = 'cross';
     } else {
         event.target.classList.add('cell__cross');
         document.querySelector('#menu__hraje-icon').classList.remove('menu__hraje-cross')
         document.querySelector('#menu__hraje-icon').classList.add('menu__hraje-circle')
-        currentPlayer = 'circle';
+        playingPlayer = 'circle';
     }
 
-    event.target.disabled = true
+    event.target.disabled = true;
 }
 
 
-document.querySelector('#cell1').addEventListener('click', whenClicked);
+document.querySelector('#cell1').addEventListener('click', cellClick);
 
-document.querySelector('#cell2').addEventListener('click', whenClicked);
+document.querySelector('#cell2').addEventListener('click', cellClick);
 
-document.querySelector('#cell3').addEventListener('click', whenClicked);
+document.querySelector('#cell3').addEventListener('click', cellClick);
 
-document.querySelector('#cell4').addEventListener('click', whenClicked);
+document.querySelector('#cell4').addEventListener('click', cellClick);
 
-document.querySelector('#cell5').addEventListener('click', whenClicked);
+document.querySelector('#cell5').addEventListener('click', cellClick);
 
-document.querySelector('#cell6').addEventListener('click', whenClicked);
+document.querySelector('#cell6').addEventListener('click', cellClick);
 
-document.querySelector('#cell7').addEventListener('click', whenClicked);
+document.querySelector('#cell7').addEventListener('click', cellClick);
 
-document.querySelector('#cell8').addEventListener('click', whenClicked);
+document.querySelector('#cell8').addEventListener('click', cellClick);
 
-document.querySelector('#cell9').addEventListener('click', whenClicked);
+document.querySelector('#cell9').addEventListener('click', cellClick);
 
-document.querySelector('#cell10').addEventListener('click', whenClicked);
+document.querySelector('#cell10').addEventListener('click', cellClick);
 
 
-const whenRefreshed = (event) => {
+const pressReplay = (event) => {
     const result = window.confirm('Hrát znovu?');
     if (result === true) {
         window.open("hra.html", 'Refresh..');
@@ -49,4 +49,4 @@ const whenRefreshed = (event) => {
       }
 }
 
-document.querySelector('#button-replay').addEventListener('click', whenRefreshed)
+document.querySelector('#button-replay').addEventListener('click', pressReplay)
